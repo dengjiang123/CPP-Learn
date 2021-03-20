e2bbc7ca767b469265c630d8871acd27832dbcea
dengjiang123 <47911802+dengjiang123@users.noreply.github.com>
2021-03-20 22:53:30 +0800

    Update demo.cpp

--- Maze/demo.cpp
+++ Maze/demo.cpp
@@ -2,6 +2,26 @@
 #include"maze.h"
 using namespace std;
 int main() {
+	int n = 70;
+	Maze All(n);
+	//All.Init();
+	All.Rand_M();
+	All.Out_M();
+	if (!All.Find_Path(0, 0, n - 1, n - 1))
+		cout << "没有找到!\n";
+	else
+		All.Out_Path();
+	All.Out_M();
+}
+
+
+
+
+/*
+#include<iostream>
+#include"maze.h"
+using namespace std;
+int main() {
 	Maze All(20);
 	//All.init();
 	All.Rand_M();
@@ -10,3 +30,5 @@ int main() {
 		cout << "没有找到!\n";
 	All.Out_M();
 }
+*/
+
